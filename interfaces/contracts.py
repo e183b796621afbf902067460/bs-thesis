4eaba9d2ff6f9b1821f8f76cd1c363e65b549089
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-08-12 17:53:31 +0300

    Added interfaces/

---
+++ interfaces/contracts.py
@@ -0,0 +1,16 @@
+from abc import ABC
+from web3 import Web3
+
+
+class ContractInterface(ABC):
+
+    def __init__(
+            self,
+            address: str, abi: str, provider: Web3.HTTPProvider
+    ) -> None:
+        self.w3 = Web3(provider)
+        self.contract = self.w3.eth.contract(address=Web3.toChecksumAddress(value=address), abi=abi)
+
+    @property
+    def address(self) -> str:
+        return self.contract.address
