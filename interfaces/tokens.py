bd99f858f979fd9cd25a6c38a422e585f7a361fb
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-08-13 18:49:27 +0300

    Added ERC20Token

---
+++ interfaces/tokens.py
@@ -0,0 +1,22 @@
+from contracts import ContractInterface
+
+
+class ERC20TokenContract(ContractInterface):
+
+    def allowance(self, account: str, spender: str) -> int:
+        return self.contract.functions.allowance(account, spender).call()
+
+    def balanceOf(self, address: str) -> int:
+        return self.contract.functions.balanceOf(address).call()
+
+    def decimals(self) -> int:
+        return self.contract.functions.decimals().call()
+
+    def name(self) -> str:
+        return self.contract.functions.name().call()
+
+    def symbol(self) -> str:
+        return self.contract.functions.symbol().call()
+
+    def totalSupply(self) -> int:
+        return self.contract.functions.totalSupply().call()
--- protocols/curve/contracts/tokens.py
+++ protocols/curve/contracts/tokens.py
@@ -1,22 +1,5 @@
-from interfaces.contracts import ContractInterface
+from interfaces.tokens import ERC20TokenContract
 
 
-class CurveTokenContract(ContractInterface):
-
-    def totalSupply(self) -> int:
-        return self.contract.functions.totalSupply().call()
-
-    def allowance(self, _owner: str, _spender: str) -> int:
-        return self.contract.functions.allowance(_owner, _spender).call()
-
-    def name(self) -> str:
-        return self.contract.functions.name().call()
-
-    def symbol(self) -> str:
-        return self.contract.functions.symbol().call()
-
-    def decimals(self) -> int:
-        return self.contract.functions.decimals().call()
-
-    def balanceOf(self, arg0: str) -> int:
-        return self.contract.functions.balanceOf(arg0).call()
+class CurveTokenContract(ERC20TokenContract):
+    pass
