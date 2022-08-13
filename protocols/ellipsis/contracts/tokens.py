e4d56668a3eb94275b89c7ff2abb79fefc876149
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-08-13 21:18:14 +0300

    Changed EllipsisTokenContract name

--- protocols/ellipsis/contracts/tokens.py
+++ protocols/ellipsis/contracts/tokens.py
@@ -1,7 +1,7 @@
 from protocols.curve.contracts.tokens import CurveLPTokenContract
 
 
-class EllipsisTokenContract(CurveLPTokenContract):
+class EllipsisLPTokenContract(CurveLPTokenContract):
 
     def minter(self) -> str:
         return self.contract.functions.minter().call()
