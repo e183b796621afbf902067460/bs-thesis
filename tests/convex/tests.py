455673c5b7d0fc883ae6b8a8978f483f727db4ed
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-08-31 13:21:06 +0300

    Added tests for ConvexVirtualBalanceRewardPool

--- tests/convex/tests.py
+++ tests/convex/tests.py
@@ -101,3 +101,83 @@ class TestConvexBaseRewardPoolContract:
         _address = '0x0000000000000000000000000000000000000000'
         assert isinstance(self._instance.userRewardPerTokenPaid(address=_address), int)
 
+
+class TestConvexVirtualBalanceRewardPoolContract:
+
+    _address = '0x81fCe3E10D12Da6c7266a1A169c4C96813435263'
+    _provider = BridgeConfigurator(abstractFabric=providerAbstractFabric, fabricKey='http', productKey=Chains.ETH).produceProduct()
+
+    _instance = ConvexVirtualBalanceRewardPoolContract()\
+        .setAddress(address=_address)\
+        .setProvider(provider=_provider)\
+        .create()
+
+    def testInstance(self):
+        assert isinstance(self._instance, ConvexVirtualBalanceRewardPoolContract)
+
+    def testAddress(self):
+        assert self._instance.address == self._address
+
+    def testProvider(self):
+        assert self._instance.provider == self._provider
+
+    def test_balanceOf(self):
+        _address = '0x0000000000000000000000000000000000000000'
+        assert isinstance(self._instance.balanceOf(address=_address), int)
+
+    def test_currentRewards(self):
+        assert isinstance(self._instance.currentRewards(), int)
+
+    def test_deposits(self):
+        assert isinstance(self._instance.deposits(), str)
+
+    def test_duration(self):
+        assert isinstance(self._instance.duration(), int)
+
+    def test_earned(self):
+        _address = '0x0000000000000000000000000000000000000000'
+        assert isinstance(self._instance.earned(address=_address), int)
+
+    def test_historicalRewards(self):
+        assert isinstance(self._instance.historicalRewards(), int)
+
+    def test_lastTimeRewardApplicable(self):
+        assert isinstance(self._instance.lastTimeRewardApplicable(), int)
+
+    def test_lastUpdateTime(self):
+        assert isinstance(self._instance.lastUpdateTime(), int)
+
+    def test_newRewardRatio(self):
+        assert isinstance(self._instance.newRewardRatio(), int)
+
+    def test_operator(self):
+        assert isinstance(self._instance.operator(), str)
+
+    def test_periodFinish(self):
+        assert isinstance(self._instance.periodFinish(), int)
+
+    def test_queuedRewards(self):
+        assert isinstance(self._instance.queuedRewards(), int)
+
+    def test_rewardPerToken(self):
+        assert isinstance(self._instance.rewardPerToken(), int)
+
+    def test_rewardPerTokenStored(self):
+        assert isinstance(self._instance.rewardPerTokenStored(), int)
+
+    def test_rewardRate(self):
+        assert isinstance(self._instance.rewardRate(), int)
+
+    def test_rewardToken(self):
+        assert isinstance(self._instance.rewardToken(), str)
+
+    def test_rewards(self):
+        _address = '0x0000000000000000000000000000000000000000'
+        assert isinstance(self._instance.rewards(address=_address), int)
+
+    def test_totalSupply(self):
+        assert isinstance(self._instance.totalSupply(), int)
+
+    def test_userRewardPerTokenPaid(self):
+        _address = '0x0000000000000000000000000000000000000000'
+        assert isinstance(self._instance.userRewardPerTokenPaid(address=_address), int)
