2ad18f50dd91a112d9fea5f92b527743e4215f1f
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-18 06:15:38 +0500

    Added TestUniSwapV3PoolContract

--- tests/uniswap/tests.py
+++ tests/uniswap/tests.py
@@ -1,15 +1,24 @@
+import uuid
+
 import pytest
 
 from defi.protocols.uniswap.contracts.UniswapV2Pair import UniSwapV2PairContract
+from defi.protocols.uniswap.contracts.UniswapV3Pool import UniSwapV3PoolContract
 
 from head.bridge.configurator import BridgeConfigurator
 from providers.abstracts.fabric import providerAbstractFabric
+
+import ctypes
 
 
 class TestUniSwapV2PairContract:
 
     _address = '0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc'
-    _provider = BridgeConfigurator(abstractFabric=providerAbstractFabric, fabricKey='http', productKey='eth').produceProduct()
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='eth'
+    ).produceProduct()
 
     _instance = UniSwapV2PairContract()\
         .setAddress(address=_address)\
@@ -79,3 +88,85 @@ class TestUniSwapV2PairContract:
 
     def test_totalSupply(self):
         assert isinstance(self._instance.totalSupply(), int)
+
+
+class TestUniSwapV3PoolContract:
+
+    _address = '0xA374094527e1673A86dE625aa59517c5dE346d32'
+    _provider = BridgeConfigurator(
+        abstractFabric=providerAbstractFabric,
+        fabricKey='http',
+        productKey='matic'
+    ).produceProduct()
+
+    _instance = UniSwapV3PoolContract()\
+        .setAddress(address=_address)\
+        .setProvider(provider=_provider)\
+        .create()
+
+    def testInstance(self):
+        assert isinstance(self._instance, UniSwapV3PoolContract)
+
+    def testAddress(self):
+        assert self._instance.address == self._address
+
+    def testProvider(self):
+        assert self._instance.provider == self._provider
+
+    def test_factory(self):
+        assert isinstance(self._instance.factory(), str)
+
+    def test_fee(self):
+        assert isinstance(self._instance.fee(), int)
+
+    def test_feeGrowthGlobal0X128(self):
+        assert isinstance(self._instance.feeGrowthGlobal0X128(), int)
+
+    def test_feeGrowthGlobal1X128(self):
+        assert isinstance(self._instance.feeGrowthGlobal1X128(), int)
+
+    def test_liquidity(self):
+        assert isinstance(self._instance.liquidity(), int)
+
+    def test_maxLiquidityPerTick(self):
+        assert isinstance(self._instance.maxLiquidityPerTick(), int)
+
+    def test_observations(self):
+        _i = 0
+        assert isinstance(self._instance.observations(i=0), list)
+
+    def test_observe(self):
+        _secondsAgos = [0]
+        assert isinstance(self._instance.observe(secondsAgos=_secondsAgos), list)
+
+    def test_positions(self):
+        _i = bytes('sadf'.encode())
+        assert isinstance(self._instance.positions(i=_i), list)
+
+    def test_protocolFees(self):
+        assert isinstance(self._instance.protocolFees(), list)
+
+    def test_slot0(self):
+        assert isinstance(self._instance.slot0(), list)
+
+    def test_snapshotCumulativesInside(self):
+        _tickLower = -277990
+        _tickUpper = -277590
+        assert isinstance(self._instance.snapshotCumulativesInside(tickLower=_tickLower, tickUpper=_tickUpper), list)
+
+    def test_tickBitmap(self):
+        _i = 0
+        assert isinstance(self._instance.tickBitmap(i=_i), int)
+
+    def test_tickSpacing(self):
+        assert isinstance(self._instance.tickSpacing(), int)
+
+    def test_ticks(self):
+        _i = 0
+        assert isinstance(self._instance.ticks(i=_i), list)
+
+    def test_token0(self):
+        assert isinstance(self._instance.token0(), str)
+
+    def test_token1(self):
+        assert isinstance(self._instance.token1(), str)
