63480f2f5f47730b1730834f13de746388899afe
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2022-12-20 15:42:25 +0500

    Updated setup

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ with open('requirements.txt') as requirements:
     required = requirements.read().splitlines()
 
 setup(
-    name="defi-contracts-evm",
+    name="defi-web3",
     version="1.0",
     author="e183b796621afbf902067460",
     author_email="606d18446a06fe9738fd@gmail.com",
-    url="https://github.com/e183b796621afbf902067460/defi-contracts-evm",
+    url="https://github.com/e183b796621afbf902067460/defi-web3",
     packages=find_packages(),
     long_description=long_description,
     install_requires=required,
