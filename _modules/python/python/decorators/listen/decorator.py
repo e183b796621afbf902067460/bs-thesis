48c791e6dbadc19fcaa980dcdc93b953fbeebabc
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-07-07 21:03:06 +0000

    Updated listen decorator in python module

--- _modules/python/python/decorators/listen/decorator.py
+++ _modules/python/python/decorators/listen/decorator.py
@@ -2,24 +2,11 @@ from functools import wraps
 from typing import Callable, Iterable
 
 
-def listen(func: Callable):
-
-    def parse(event: list):
-        return {
-            'address': event[0],
-            'dt': event[1],
-            't0_symbol': event[2],
-            't1_symbol': event[3],
-            't0_amount': event[4],
-            't1_amount': event[5],
-            'tx_hash': event[6],
-            'protocol': event[7],
-            'blockchain': event[8]
-        }
+def listen(func: Callable, parse_func: Callable):
 
     @wraps(func)
     def wrapper(*args, **kwargs) -> Iterable:
         while True:
             for event in func(*args, **kwargs):
-                yield parse(event=event)
+                yield parse_func(event)
     return wrapper
