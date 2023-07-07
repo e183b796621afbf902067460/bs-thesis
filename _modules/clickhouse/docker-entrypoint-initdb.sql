67842acd62d64409a095dc621bf1d7ae8f529194
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-07-07 22:14:35 +0000

    Updated clickhouse module

--- _modules/clickhouse/docker-entrypoint-initdb.sql
+++ _modules/clickhouse/docker-entrypoint-initdb.sql
@@ -1,3 +1,25 @@
+-- Actual table to store the data fetched from an Apache Kafka topic
+CREATE TABLE IF NOT EXISTS clickhouse.dm_real_time_tx_processing
+(
+    q_real_time_tx_processing_address           String,
+
+    q_real_time_tx_processing_t0_symbol         String,
+    q_real_time_tx_processing_t1_symbol         String,
+    q_real_time_tx_processing_t0_amount         Float64,
+    q_real_time_tx_processing_t1_amount         Float64,
+
+    q_real_time_tx_processing_tx_hash           String,
+
+    q_real_time_tx_processing_protocol          String,
+    q_real_time_tx_processing_blockchain        String,
+
+    q_real_time_tx_processing_timestamp         DateTime
+)
+ENGINE = MergeTree
+PARTITION BY toYYYYMMDD(q_real_time_tx_processing_timestamp)
+ORDER BY q_real_time_tx_processing_timestamp;
+
+-- Kafka Engine which consumes the data from 'real.time.tx.processing' of Apache Kafka
 CREATE TABLE IF NOT EXISTS clickhouse.q_real_time_tx_processing
 (
     q_real_time_tx_processing_address           String,
@@ -20,3 +42,18 @@ SETTINGS
     kafka_topic_list = 'real.time.tx.processing',
     kafka_group_name = 'q_real_time_tx_processing_result',
     kafka_format = 'JSONEachRow';
+
+-- Materialized View to insert any consumed data by Kafka Engine to 'dm_real_time_tx_processing' table
+CREATE MATERIALIZED VIEW clickhouse.mv_real_time_tx_processing TO clickhouse.dm_real_time_tx_processing AS
+SELECT
+    q_real_time_tx_processing_address,
+    q_real_time_tx_processing_t0_symbol,
+    q_real_time_tx_processing_t1_symbol,
+    q_real_time_tx_processing_t0_amount,
+    q_real_time_tx_processing_t1_amount,
+    q_real_time_tx_processing_tx_hash,
+    q_real_time_tx_processing_protocol,
+    q_real_time_tx_processing_blockchain,
+    q_real_time_tx_processing_timestamp
+FROM
+    clickhouse.q_real_time_tx_processing;
