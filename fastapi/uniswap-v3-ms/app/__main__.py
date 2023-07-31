b50ff5e1a84bc57e36a9da2fe0f8615867b48c56
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-07-31 20:05:29 +0000

    Added 0x0a6c4588b7D8Bd22cF120283B1FFf953420c45F3 router

--- fastapi/uniswap-v3-ms/app/__main__.py
+++ fastapi/uniswap-v3-ms/app/__main__.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 
 from app.resources.env.resource import spawn_env_resource
-from app.views.A374094527e1673A86dE625aa59517c5dE346d32.view import router as A374094527e1673A86dE625aa59517c5dE346d32
+
+from app.views.xA374094527e1673A86dE625aa59517c5dE346d32.view import router as xA374094527e1673A86dE625aa59517c5dE346d32
+from app.views.x88f3C15523544835fF6c738DDb30995339AD57d6.view import router as x88f3C15523544835fF6c738DDb30995339AD57d6
+from app.views.x0a6c4588b7D8Bd22cF120283B1FFf953420c45F3.view import router as x0a6c4588b7D8Bd22cF120283B1FFf953420c45F3
 
 
 app, env = FastAPI(), spawn_env_resource()
 
 
-app.include_router(router=A374094527e1673A86dE625aa59517c5dE346d32, prefix=f'{env.api_v1}' + f'{env.root}')
+app.include_router(router=xA374094527e1673A86dE625aa59517c5dE346d32, prefix=f'{env.api_v1}' + f'{env.root}')
+app.include_router(router=x88f3C15523544835fF6c738DDb30995339AD57d6, prefix=f'{env.api_v1} + f{env.root}')
+app.include_router(router=x0a6c4588b7D8Bd22cF120283B1FFf953420c45F3, prefix=f'{env.api_v1} + f{env.root}')
 
 
 if __name__ == '__main__':
