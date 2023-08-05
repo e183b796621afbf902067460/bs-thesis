a2293532ccba96bf67720079ff4eaf4e32af6feb
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-08-06 00:30:02 +0200

    Updated quickswap-v3-ms

--- fastapi/quickswap-v3-ms/app/__main__.py
+++ fastapi/quickswap-v3-ms/app/__main__.py
@@ -1,12 +1,25 @@
+from typing import Callable, List
+
 from fastapi import FastAPI
 
-from app.views.x55CAaBB0d2b704FD0eF8192A7E35D8837e678207.view import router as x55CAaBB0d2b704FD0eF8192A7E35D8837e678207
+from python.decorators.background.decorator import background
+
+from app.views.x55CAaBB0d2b704FD0eF8192A7E35D8837e678207.view import broadcast as x55CAaBB0d2b704FD0eF8192A7E35D8837e678207
+
+
+pools = list()
+pools.append(x55CAaBB0d2b704FD0eF8192A7E35D8837e678207)
+
+
+@background
+def include(pool: Callable) -> None: pool()
 
 
 app = FastAPI()
 
 
-app.include_router(router=x55CAaBB0d2b704FD0eF8192A7E35D8837e678207)
+@app.on_event('startup')
+async def startup(pools: List[Callable] = pools) -> None: [include(pool) for pool in pools]
 
 
 if __name__ == '__main__':
