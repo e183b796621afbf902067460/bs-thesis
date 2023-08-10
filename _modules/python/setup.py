15e7b6e6b8fd7bca33b830616f39bfcb5e308c74
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-08-11 00:37:26 +0200

    Updated setup in python module

--- _modules/python/setup.py
+++ _modules/python/setup.py
@@ -9,10 +9,10 @@ with open('requirements.txt') as requirements:
 
 setup(
     name="python",
-    version="0.0.4",
+    version="0.0.5",
     author="e183b796621afbf902067460",
     author_email="606d18446a06fe9738fd@gmail.com",
-    url="https://github.com/e183b796621afbf902067460/bs-thesis/tree/master/_modules/python",
+    url="https://github.com/e183b796621afbf902067460/baccalaureate-diploma/tree/master/_modules/python",
     packages=find_packages(
         exclude=['python_tests*']
     ),
