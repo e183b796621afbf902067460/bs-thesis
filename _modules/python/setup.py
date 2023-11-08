90e18254eb6790127f05e9466020e14e5105157a
e183b796621afbf902067460 <606d18446a06fe9738fd@gmail.com>
2023-11-08 13:29:37 +0100

    Updated namning

--- _modules/python/setup.py
+++ _modules/python/setup.py
@@ -12,7 +12,7 @@ setup(
     version="0.0.5",
     author="e183b796621afbf902067460",
     author_email="606d18446a06fe9738fd@gmail.com",
-    url="https://github.com/e183b796621afbf902067460/baccalaureate-diploma/tree/master/.modules/python",
+    url="https://github.com/e183b796621afbf902067460/baccalaureate-diploma/tree/master/_modules/python",
     packages=find_packages(
         exclude=['python_tests*']
     ),
